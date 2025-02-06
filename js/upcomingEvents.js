849ba4851cdf3ea677f861f7b8dbca49a9777e41
ShouvikSarkar1 <shouviksarkarmusic@gmail.com>
2025-02-06 16:46:36 -0500

    event_box update

--- js/upcomingEvents.js
+++ js/upcomingEvents.js
@@ -1,15 +1,24 @@
 const events = [
   {
     id: 1,
+    title: "My Wedding",
+    description: "I am getting married :')",
+    location: "The Boathouse Rocketts Landing Richmond, VA",
+    date: "11/01/2025",
+    time: "04:30PM EST",
+    image: 'images/ED3A9021.jpg'
+  },
+  {
+    id: 2,
     title: "JavaScript Conference",
     description: "JavaScript developer meetup.",
-    location: "VCU Snead Hall",
+    location: "VCU Snead Hall Richmond, VA",
     date: "03/14/2025",
     time: "10:00AM EST",
     image: "https://learn.g2.com/hubfs/Pillar%20Pages/event%20marketing/event%20branding/event-branding.png"
   },
   {
-    id: 2,
+    id: 3,
     title: "SuperBowl LIX",
     description: "Kansas City Chiefs vs. Philadelphia Eagles",
     location: "New Orleans, LA",
@@ -18,7 +27,7 @@ const events = [
     image: "https://www.tennessean.com/gcdn/authoring/images/smg/2024/07/09/USAT/74334982007-2005701712-1-1_1.jpeg?crop=4901,2758,x0,y310&width=1600&height=800&format=pjpg&auto=webp"
   },
   {
-    id: 3,
+    id: 4,
     title: "Mini Project 1",
     description: "Your project is due soon",
     location: "Richmond, VA",
@@ -27,7 +36,7 @@ const events = [
     image: "https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1024"
   },
   {
-    id: 4,
+    id: 5,
     title: "Deftones",
     description: "Deftones live",
     location: "Capital One Arena Washington, D.C.",
@@ -35,6 +44,7 @@ const events = [
     time: "07:00PM EST",
     image: "https://www.rollingstone.com/wp-content/uploads/2018/06/rs-234646-photo-credit-frank-maddocks-extralarge_1457569542368.jpg?w=1600&h=900&crop=1"
   },
+ 
 ]
 
 export default events;
